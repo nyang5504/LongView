54d1130763537051d907e12259c8788576095ea2
nyang5504 <yang.nina5504@gmail.com>
2026-06-19 11:55:41 -0400

    Initial SpringBoot project

---
+++ src/main/java/com/nyang5504/LongView/LongViewApplication.java
@@ -0,0 +1,13 @@
+package com.nyang5504.LongView;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LongViewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LongViewApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nyang5504/LongView/LongViewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nyang5504.LongView;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LongViewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
